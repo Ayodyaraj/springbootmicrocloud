3e9bba14b90669f0ba8b595bd1c9a66889f51b9d
PAyodya <PAyodya@BHAVNAWKS579.BHAVNACORP.NET>
2021-07-22 12:36:11 +0530

    First Commit RepoConfig

---
+++ repoconfig-server/src/test/java/com/bhavnacorp/repoconfig/server/RepoconfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhavnacorp.repoconfig.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepoconfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
